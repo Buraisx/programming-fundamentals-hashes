c4b013c917c617f3c7e2226f892b7957244d344b
Situ William <situ_william@yahoo.com>
2017-11-01 15:11:35 -0400

    finished exercise1 using hashes

--- exercise1.rb
+++ exercise1.rb
@@ -1,16 +1,19 @@
-
+# This function takes the percentage to convert to a letter
 def calc_grades(percentage)
 	plusminus = percentage%10 #Checks remainder to determine ending symbol 95/10 r=5
 	tag = "" #The ending symbol
 	# Collection of grades
 	grade = {80..100 => "A",70..79 => "B", 60..69 => "C", 50..59 => "D", 0..49 => "F"}
-	
+	#loops through collection
 	grade.each do |mark, letter| 
+		#checks if PERCENTAGE belongs inside the range of MARK using ===
 		if mark === percentage
+			#checking exceptionals of A+ and F
 			if percentage >85
 				tag = "+"
 			elsif percentage <50
 				tag = ""
+			#Normal check for every other grade
 			elsif plusminus>5
 				tag = "+"
 			elsif plusminus<5
@@ -21,7 +24,8 @@ def calc_grades(percentage)
 	end
 end
 
+# Prompts user for an input
 puts "Enter a percentage."
 percentage = gets.chomp
-puts"Your mark is #{calc_grades(percentage.to_i)}"
-# puts "Your mark is #{calc_grades(percentage.to_i)}"
+#Calls method to output Converted grade.
+puts "Your mark is #{calc_grades(percentage.to_i)}"
