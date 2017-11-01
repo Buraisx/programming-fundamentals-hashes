fc5cf616c6289e443efc97d1c9c3099d66e6fc37
Situ William <situ_william@yahoo.com>
2017-11-01 15:55:31 -0400

    added arrays and hashes for the exercise

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+# Initializing Arrays
+fav_colors = ["blue", "black","white","red"]
+peer_ages = [22,24,22,25]
+coin_flip = ["tails","tails","tails","heads","heads",]
+artists = ["Lily Allen", "Idina Menzel","Kristen Chenoweth"]
+symbol_colors = [:blue, :black,:white,:red]
+
+#Initializing Hashes
+three_words = { :genderfluid => "relating to, or a being whose gender is not fixed; a mix of both genders",
+				:Neanderthal => "a species of subspecies of archaic humans that became extinct 40000 years ago",
+				:Homunculus => "a very small human or humanoid creature"}
+fav_movies = {"Shaun of the Dead" => 2004, "Logan" => 2017, "National Treasure" => 2004}
+three_cities = {"Toronto" => 2.809*10**6, "Tokyo" => 9.273*10**6, "New York" => 8.538*10**6}
+peer_names = {"Allen" => 22, "Valerie" => 24, "Calvin" => 22, "Abnoor" => 25}
